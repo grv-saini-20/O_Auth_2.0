1dfd8592daca6f8d3c171c937239c0088c4a1cbd
gourav <gourav@auvo.io>
2026-04-03 11:26:21 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
+
+export default app;
